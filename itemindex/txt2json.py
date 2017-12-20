8f3d89bc5d893ba7241b0d453c6f7ea2eb1c6af2
Yangcyself <13520183356@139.com>
2017-12-20 16:41:54 +0800

    add parser functions: 1: parse name by deleting all punctuations 2: parse  reviews by jieba errors: some reviews got maximum recursions when cut with jieba. not knowing the reason

--- itemindex/txt2json.py
+++ itemindex/txt2json.py
@@ -1,8 +1,36 @@
 #-^-coding:utf-8-^-
 import os
 import json
-root = "basic_info"
-targetroot = "json_info"
+import jieba
+import re
+import string
+"""
+change the txt files into json ones 
+parse the file by the way
+"""
+errors = []
+
+def nameparser(name):
+    punctuation = (string.punctuation+"‘（）。，、；’【、】`～！￥%……&×——+=-|：”《》？").decode('utf-8')
+    tempname = ""
+    name = name.decode("utf8")
+    for i in range(len(name)):
+        if name[i] not in punctuation:
+            tempname+=name[i]
+    return tempname.encode("utf8")
+
+def contentparser(cont):
+    #print type(cont)
+    try:
+        cont = " ".join(jieba.cut(cont))
+    except:
+        print cont
+        errors.append(cont)
+        return None
+    return cont.encode("utf8")
+
+root = "../datastore/basic_info"
+targetroot = "../datastore/json_info"
 for root, dirnames, filenames in os.walk(root):
     for filename in filenames:
         if True:
@@ -17,7 +45,7 @@ for root, dirnames, filenames in os.walk(root):
                         if line[0]=="appid:":
                             game["id"] = int(line[1])
                         elif line[0]=="appname:":
-                            game["name"] = " ".join(line[1:])
+                            game["name"] = nameparser(" ".join(line[1:]))
                         elif line[0]=="apptags:":
                             game["tags"] = " ".join(line[1:])
                         elif line[0]=="appdevelopers:":
@@ -29,10 +57,11 @@ for root, dirnames, filenames in os.walk(root):
                         #print 'helre'
                         game["review"]= game["review"]+line+" "
             path = os.path.join(targetroot,filename+".json")
-            #print game
+            game["review"] = contentparser(game["review"])
+            print game["id"] 
             with open(path,"w") as f:
                 json.dump(game,f,ensure_ascii=False)
-            break
 
+print errors
 
                                 
