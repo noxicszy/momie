7d54d4be57ac975507ee51fcf66ec94600eb36f2
Yangcyself <13520183356@139.com>
2018-01-02 21:59:59 +0800

    added dumping files

--- itemindex/nlp_vector.py
+++ itemindex/nlp_vector.py
@@ -9,6 +9,7 @@ import sys, os, lucene, threading, time
 from org.apache.lucene.util import Version
 from org.apache.lucene.analysis.core import SimpleAnalyzer,WhitespaceAnalyzer
 import json
+import pickle
 
 
 from gameindexer import IndexFiles
@@ -30,6 +31,9 @@ with open("sentiment_score.txt","r") as f:
 #print sentimentdic["好"]
 
 root = "../datastore/duple_words"
+count = 0
+data = {}
+datadir = "../datastore/vector_info"
 for root, dirnames, filenames in os.walk(root):
     for filename in filenames:
         if not filename.endswith('.json') and not filename.endswith('.JSON'):
@@ -41,7 +45,7 @@ for root, dirnames, filenames in os.walk(root):
         if not contents:
             continue
         vector = [0]*len(KeyWords)
-        cont = 0
+        cont = 0.001
         for k in contents.values():
             for tup in k:
                 cont+=1
@@ -50,8 +54,18 @@ for root, dirnames, filenames in os.walk(root):
                         vector[i]+=mod.similarity(tup[0],KeyWords[i].decode("utf8"))*sentimentdic.get(tup[1].encode("utf8"),0)
                         # print mod.similarity(tup[0],KeyWords[i].decode("utf8")),sentimentdic.get(tup[1].encode("utf8"),0)
         vector = [2*i/cont for i in vector]
-        print filename.split(".")[0], vector
-        
+        count+=1
+        data[int(filename.split(".")[0])] = vector
+        print filename.split(".")[0],count #这个速度3万个大概要跑两天（不过可以分到不同文件夹里面去跑）
+        if count%200 == 0:
+            with open(os.path.join(datadir,"{}.pkl".format(count/200)),"wb") as f:
+                pickle.dump(data,f)
+                data = {}
+with open(os.path.join(datadir,"{}.pkl".format(count/200)),"wb") as f:
+    pickle.dump(data,f)
+
+
+
 
 
             
