a3c7a0cf1add53c199c59cc9a58f176a3067960e
Noxic <noxicszy@sjtu.edu.cn>
2018-01-06 23:09:43 +0800

    Image search completes

--- webfetch/picture_query/ImageSet.py
+++ webfetch/picture_query/ImageSet.py
@@ -8,8 +8,10 @@ import pickle
 
 class ImageSet:
     def __init__(self):
-        self.N = 8
-        self.dimension = self.N**2
+        self.x_dimension = 10
+        self.y_dimension = 10
+        self.division = 5
+        self.dimension = self.x_dimension * self.y_dimension
         if not os.path.exists('baskets.pkl'):
             self.baskets = {}
         else:
@@ -22,7 +24,7 @@ class ImageSet:
             with open('appids.pkl', 'rb') as pickle_file:
                 self.appids = pickle.load(pickle_file)
 
-    def dumpall():
+    def dumpall(self):
         with  open('baskets.pkl', 'wb') as pickle_file:
             pickle.dump(self.baskets, pickle_file)
         with  open('appids.pkl', 'wb') as pickle_file:
@@ -34,31 +36,45 @@ class ImageSet:
             for x in range(len(image[0])):
                 for color in range(3):
                     BGR[color] += image[y][x][color]
-        BGR /= np.sum(BGR)
+        BGR = BGR/np.sum(BGR)
         return BGR
 
     def getFeatures(self, image):
-        image = cv2.resize(image, (self.N, self.N))
+        image = cv2.resize(image, (self.x_dimension, self.y_dimension))
         features = np.array([]).astype(np.uint8)
         for y in range(len(image)):
             for x in range(len(image[0])):
-                features = np.hstack((features, image[y][x].astype(np.uint8)))
+                features = np.hstack(( features, image[y][x].astype(np.uint8) ))
         return features
-    
+
+
+        # /(255/self.division+1) 
+    # def quantize(self, features):
+        # p = np.array([])
+        # for i in range(self.dimension):
+            # hist = features[i*3:i*3+3]
+            # MAX = max(hist)
+            # MIN = min(hist)
+            # for i in range(3):
+            #     if hist[i] == MAX:
+            #         hist[i] = 0
+            #     elif hist[i] == MIN:
+            #         hist[i] = 2
+            #     else:
+            #         hist[i] = 1
+            # p = np.hstack((p, hist))
+        # return p
+
     def quantize(self, features):
-        p = np.array([])
-        for i in range(self.dimension):
-            hist = features[i*3:i*3+3]
-            MAX = max(hist)
-            MIN = min(hist)
-            for i in range(3):
-                if hist[i] == MAX:
-                    hist[i] = 0
-                elif hist[i] == MIN:
-                    hist[i] = 2
-                else:
-                    hist[i] = 1
-            p = np.hstack((p, hist))
+        p = []
+        for i in range(self.dimension*3):
+            if features[i] > 0.3:
+                p.append(2)
+            elif features[i] > 0.25:
+                p.append(1)
+            else:
+                p.append(0)
+        p = np.array(p).astype(np.uint8)
         return p
 
     def getLSH(self, features):
@@ -82,7 +98,16 @@ class ImageSet:
             sum += projection[i]*n
             n *= 2
         return sum
-        
+        # features_dot = features/(255/self.division+1) 
+        # LSH = []
+        # sum = 0
+        # for i in range(self.dimension):
+        #     t = features_dot[i]+features_dot[i+1]+features_dot[i+2]
+            
+        #     LSH.append(t)
+        #     # sum = sum*(self.division*3-2) + t
+        # LSH = tuple(LSH)
+        # return LSH
         
     def add(self, filename, image):
         features = self.getFeatures(image)
@@ -103,7 +128,8 @@ class ImageSet:
         result = []
         for filename, features in similars:
             distance = (np.sum((target_features-features)**2))**(0.5)
-            result.append([distance, filename])
+            if distance < 160:
+                result.append([distance, filename])
         result.sort()
         for i in range(len(result)):
             result[i] = result[i][1]
@@ -120,10 +146,8 @@ class ImageSet:
         result = self.sortResult(similars, features)
         return result
             
-print 'loading imgs...'           
-imageSet = ImageSet()
-
-
+            
+            
 if __name__ == '__main__':
     imageSet = ImageSet()
     start_time = time.time()
@@ -131,4 +155,4 @@ if __name__ == '__main__':
     targetimg = cv2.imread(targetname, cv2.IMREAD_COLOR)
     similars = imageSet.getSimilar(targetimg)
     print similars
-    print time.time() - start_time
+    print time.time()-start_time
